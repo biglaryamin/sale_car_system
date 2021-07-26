a467b172048d913cea5de6819bfd6c903dd13ac2
mohammadamin <biglarymohamadamin75@gmail.com>
2021-07-26 22:56:04 +0430

    accessebility user us on like special_user and  is_author

--- account/admin.py
+++ account/admin.py
@@ -3,6 +3,16 @@ from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 
+UserAdmin.fieldsets[2][1]['fields']=('is_active',
+                                        'is_staff',
+                                        'is_superuser',
+                                        'is_author',
+                                        'special_user',
+                                        'groups',
+                                        'user_permissions',
+                                                )
 
+
+UserAdmin.list_display+=('is_author','is_special_user')
 
 admin.site.register(User,UserAdmin)
